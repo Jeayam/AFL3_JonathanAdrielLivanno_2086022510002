a3745dfc794d8f22a3bce560c7a449230106c4fe
Jeayam <jlivanno@student.ciputra.ac.id>
2026-04-30 14:12:45 +0800

    struktur

--- js/script.js
+++ js/script.js
@@ -4,19 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     if(form) {
         form.addEventListener('submit', function(e) {
+
             e.preventDefault();
             
+
             const nameInput = document.getElementById('name').value;
 
-            // 1. Tampilkan Alert browser (Syarat tugas)
+
             alert('Pesan Anda telah terkirim, ' + nameInput + '!');
 
-            // 2. Tampilkan notifikasi di halaman (Sesuai foto referensi)
+
             if(successAlert) {
                 successAlert.classList.remove('d-none');
             }
 
-            // Reset form setelah kirim
             form.reset();
         });
     }
